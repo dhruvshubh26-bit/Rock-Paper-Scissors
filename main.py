c8e8fd9966d63e89d9dd40dccabd44abb7faded6
Dhruv Sukhadiya <dhruvshubh26@gmail.com>
2026-03-08 23:50:57 +0530

    Final

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
 import random 
 
-computer_choice = random.choice([0,1,2]) #0 For Rock 1 For Paper 2 For Scissors
+computer_choice_int = random.choice([0,1,2]) #0 For Rock, 1 For Paper, 2 For Scissors
 
-mine_choice = int(input("Enter your choice : "))
+mine_choice_str = input("Enter your choice : ") #'r' for Rock ,'p' for Paper, 's' for Scissors
 
-if(computer_choice==mine_choice):
-    print("Draw")
+mine_choice_dict={'r':0,'p':1,'s':2}
+
+rev_choice_dict={0:'Rock',1:'Paper',2:"Scissors"}
+
+mine_choice_int=mine_choice_dict[mine_choice_str]
+
+mine_choice=rev_choice_dict[mine_choice_int]
+computer_choice = rev_choice_dict[computer_choice_int]
+print(f"You choose {mine_choice} and computer choose {computer_choice}")
+
+if(computer_choice_int==mine_choice_int):
+    print("\nDraw")
 else:
-    if(computer_choice==0 and mine_choice==1):
-        print("You Won!")
-    elif(computer_choice==0 and mine_choice==2):
-        print("You loose!")
-    elif(computer_choice==1 and mine_choice==0):
-        print("You lose!")
-    elif(computer_choice==1 and mine_choice==2):
-        print("You won!")
-    elif(computer_choice==2 and mine_choice==0):
-        print("You won!")
-    elif(computer_choice==2 and mine_choice==1):
-        print("You lose!")
+    if(computer_choice_int==0 and mine_choice_int==1):
+        print("\nYou Won!")
+    elif(computer_choice_int==0 and mine_choice_int==2):
+        print("\nYou loose!")
+    elif(computer_choice_int==1 and mine_choice_int==0):
+        print("\nYou lose!")
+    elif(computer_choice_int==1 and mine_choice_int==2):
+        print("\nYou won!")
+    elif(computer_choice_int==2 and mine_choice_int==0):
+        print("\nYou won!")
+    elif(computer_choice_int==2 and mine_choice_int==1):
+        print("\nYou lose!")
     
